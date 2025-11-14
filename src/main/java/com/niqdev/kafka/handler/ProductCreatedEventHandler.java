63e475bf7217a41859d7b881504a285f0693963d
Nick Chen <nick.chen@cmp.com.tw>
2025-11-14 09:25:09 +0800

    feat: add shared event and handler

---
+++ src/main/java/com/niqdev/kafka/handler/ProductCreatedEventHandler.java
@@ -0,0 +1,22 @@
+package com.niqdev.kafka.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import com.niqdev.kafka.shared.event.ProductCreatedEvent;
+
+@Component
+@KafkaListener(topics = "product-created-events-topic")
+public class ProductCreatedEventHandler {
+	
+	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+	@KafkaHandler
+	public void handle(ProductCreatedEvent productCreatedEvent) {
+		LOGGER.info("Received a new event: " + productCreatedEvent.getTitle());
+	}
+	
+}
